d58a2cf520134fe50d9c8ea52e2f15a16497b835
azu <azuciao@gmail.com>
2017-05-02 00:41:23 +0900

    removed

--- examples/src/index.js
+++
@@ -1,7 +0,0 @@
-// LICENSE : MIT
-"use strict";
-import "babel-polyfill"
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./component/App";
-ReactDOM.render(<App />, document.getElementById("js-app"));
--- examples/src/usecase/ThrowErrorUseCase.js
+++
@@ -1,8 +0,0 @@
-// MIT © 2017 azu
-"use strict";
-import { UseCase } from "almin";
-export default class ThrowErrorUseCase extends UseCase {
-    execute() {
-        this.throwError(new Error("ERR!"));
-    }
-}
